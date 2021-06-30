6b1f6fe90dfd7b8352fdb95d166e9c7a74b7c47e
Moeyuuko <zihao890@gmail.com>
2021-06-30 14:23:34 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# gdlgxy-ElecMeter
+# 广东理工学院-宿舍电表数据
+通过小程序接口, 拉取高要校区宿舍电费信息 的python脚本.抄表间隔很长, 没必要每秒拉取. 推荐间隔30分钟~1小时一次, 请勿滥用接口
+
+## 使用
+- ### import导入 GetRoomInfo(Roomname) 返回字典Dictionary
+	```python
+	import power_get.py
+
+	data = power_get.GetRoomInfo("16621")
+	if data['state']=='ok':   #建议判断返回状态是否ok再往下执行
+	```
+
+- ### 可直接执行 返回json 可同时获取多个宿舍
+	```Shell
+	python3 power_get.py 16621 16612 17101
+	```
+	```json
+	{'state': 'ok', 'name': '16621', 'Used': '8169.97', 'Remaining': '19.06', 'RecordTime': '2021/6/30 12:07:16', 'TimeStamp': '1625026036'}
+	{'state': 'ok', 'name': '16612', 'Used': '8680.66', 'Remaining': '44.87', 'RecordTime': '2021/6/30 12:34:42', 'TimeStamp': '1625027682'}
+	{'state': 'ok', 'name': '17101', 'Used': '5209.58', 'Remaining': '21.78', 'RecordTime': '2021/6/30 11:55:26', 'TimeStamp': '1625025326'}
+	```
---
+++ power_get.py
@@ -0,0 +1,71 @@
+#!/bin/python3
+#-*- coding: UTF-8 -*-
+import sys
+import requests
+import traceback
+import time
+from lxml import etree
+
+api = "http://www.gyruibo2.cn/WxSearch/GetRoomInfo"
+
+header = {
+	'User-Agent': 'Mozilla/5.0 (Linux; Android 11; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2691 MMWEBSDK/200901 Mobile Safari/537.36 MMWEBID/419 MicroMessenger/7.0.19.1760(0x27001380) Process/toolsmp WeChat/arm64 NetType/2G Language/zh_CN ABI/arm64'
+}
+
+Apartid = {
+			"10": "8170",
+			"11": "8003",
+			"12": "8003",
+			"13": "8006",
+			"14": "8006",
+			"15": "8007",
+			"16": "8008",
+			"17": "8009",
+			"18": "8163",
+			"19": "8010",
+			"20": "8011",
+			"21": "8012",
+			"22": "8013",
+			"23": "8014",
+			"24": "8015",
+			"28": "8016",
+			"29": "8157",
+			"34": "8018",
+			"40": "8019",
+			"43": "8020",
+			"45": "8021",
+			"46": "8022",
+			"47": "8023",
+			"48": "8024",
+			"49": "8304"
+			}
+
+def GetRoomInfo(Roomname):
+	session = requests.session()
+	session.headers = header
+	try:
+		api_get = api + "?SchID=1&Apartid=" + Apartid[Roomname[0:2]] +"&Roomname=" + Roomname
+		html_data = session.get(str(api_get))
+		#print (html_data.text)
+		raw_data = etree.HTML(html_data.text)
+		Room_data = {
+			'state' : 'ok',
+			'name' : raw_data.xpath('//div/div/div/label/text()')[0].replace('\r\n','').strip(),
+			'Used' : raw_data.xpath('//div/div/div/label/text()')[1].replace('\r\n','').replace('度','').strip(),
+			'Remaining'  : raw_data.xpath('//div/div/div/label/text()')[2].replace('\r\n','').replace('度','').strip(),
+			'RecordTime'  : raw_data.xpath('//div/div/div/label/text()')[3].replace('\r\n','').strip(),
+			'TimeStamp'  : str(round(time.mktime(time.strptime(raw_data.xpath('//div/div/div/label/text()')[3].replace('\r\n','').strip(), "%Y/%m/%d %H:%M:%S"))))		#转成时间戳
+			}
+		if Room_data['name'] == '':
+			Room_data['state']='null'
+
+	except :
+		Room_data = {
+			'state' : 'error',
+			'info' : traceback.format_exc()
+			}
+	return(Room_data)
+
+if __name__ == '__main__':
+	for i in range(len(sys.argv)-1):
+		print (GetRoomInfo(sys.argv[i+1]))
